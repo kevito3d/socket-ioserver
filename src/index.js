441249088f9e8e4a8cdd2c9f6d93d77697ee1bf9
kevito3d <kevincove98@gmail.com>
2023-01-18 17:18:45 -0500

    add logic bands end

--- src/index.js
+++ src/index.js
@@ -22,5 +22,5 @@ server.listen(process.env.PORT || 3000, (err) => {
   if (err) {
     console.log(err);
   }
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port: ", process.env.PORT || 3000);
 });
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,9 +1,50 @@
-const {io} = require('../index');
-io.on('connection', client => {
-    console.log('Client connected...');
-    client.on('disconnect', () => { console.log('Client disconnected') });
-    client.on('message', (payload) => {
-        console.log(payload);
-    io.emit('message', {"admin": "Nuevo mensaje"});
+const { io } = require("../index");
+const Band = require("../models/band.model");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Bon Jovi"));
+bands.addBand(new Band("Heroes del silencio"));
+bands.addBand(new Band("Metallica"));
+bands.addBand(new Band("Guns N' Roses"));
+
+console.log(bands);
+
+io.on("connection", (client) => {
+  console.log("Client connected...");
+
+  client.emit("active-bands", bands.getBands());
+
+  client.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+
+  client.on("message", (payload) => {
+    console.log(payload);
+    io.emit("message", { admin: payload });
+  });
+
+  client.on("send-message", (payload) => {
+    console.log(payload);
+    client.broadcast.emit("message", { payload });
+  });
+
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+    client.on("add-band", (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit("active-bands", bands.getBands());
     });
+
+    client.on("delete-band", (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit("active-bands", bands.getBands());
+    });
+
 });
